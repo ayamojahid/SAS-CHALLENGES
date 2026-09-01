eed8bd1722b9530b247cb53a9864f13c99d39257
Aya <aya.mojahid.2025@gmail.com>
2026-09-01 11:36:21 +0100

    la condition switch avec des exercices

--- LES-CONDITIONS.js
+++ LES-CONDITIONS.js
@@ -32,3 +32,39 @@ if (nombre > 0) {
 } else {
     console.log("Nul");
 }
+
+/*switch en JavaScript
+switch sert à choisir une action parmi plusieurs possibilités.
+C'est surtout pratique quand tu veux comparer une même variable avec plusieurs valeurs.
+Par exemple, un programme qui  reconnaître un jour :
+
+1 → lundi
+2 → mardi
+3 → mercredi
+etc.
+Avec beaucoup de if / else if, ça peut devenir long. switch permet de rendre ça plus clair.
+
+Les éléments importants
+
+switch → indique ce qu'on veut vérifier.
+case → représente une possibilité.
+break → arrête le switch une fois qu'un case a été trouvé.
+default → ce qui se passe si aucune possibilité ne correspond.*/
+
+//Exemple
+
+let jour = 5;
+switch (jour) {
+    case 1:
+        console.log("Lundi")
+        break;
+    case 2:
+        console.log("Mardi")
+        break;
+    case 3:
+        console.log("Mercredi")
+        break;
+
+    default:
+        console.log("Aucun jour")
+}
