16105f3cca668ab0ea655dac620928078597538c
Aya <aya.mojahid.2025@gmail.com>
2026-09-03 11:21:33 +0100

    les exercices de level 1

---
+++ Day2/CHALLENGES/LEVEL1/EX.js
@@ -0,0 +1,22 @@
+//1 Greeting Function: Write a standard function greet(name) that takes a string and logs 
+// "Hello, [name]!".
+
+function greet(name) {
+    console.log(`Hello, ${name}`)
+}
+greet("AYA");
+
+// 2  Arrow Addition: Write an arrow function add(a, b) that takes two 
+// numbers and returns their sum. Log the result of calling the function.
+
+function add(a,b) {
+    return a+b;
+}
+console.log(add(20,50));
+
+//3 Square Function: Write a function square(num) that returns the square of a given number.
+
+function square(a) {
+    return a*a;
+}
+console.log(square(5));
