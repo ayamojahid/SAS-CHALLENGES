e78d82d6a800ecef8df8e3213f6391494db97430
aya mojahid <aya.mojahid.2025@gmail.com>
2026-09-07 22:40:59 +0100

    exercice 1 et 2

--- Day5/challenges/LEVEL3/challenges3.js
+++ Day5/challenges/LEVEL3/challenges3.js
@@ -1,39 +1,81 @@
 //Groupe par département : Étant donné un tableau d’objets, écrivez 
 // une fonction qui les regroupe par leur propriété dans un nouvel objet.employeedepartment
+
+
+
 let depar = [
     {
-        nom : "aya" , 
-        departement : "IT"
+        nom: "aya",
+        departement: "IT"
     },
-     {
-        nom : "Mahmmoud" , 
-        departement : "IT"
+    {
+        nom: "Mahmmoud",
+        departement: "IT"
     },
-     {
-        nom : "salma" , 
-        departement : "marketing"
+    {
+        nom: "salma",
+        departement: "marketing"
     },
-     {
-        nom : "salima" , 
-        departement : "rh"
+    {
+        nom: "salima",
+        departement: "rh"
     },
-     {
-        nom : "aya" , 
-        departement : "rh"
+    {
+        nom: "hamid",
+        departement: "rh"
     }
-]
+];
+
 function departement() {
-    let stockegale;
-    for(let i = 0 ; i<depar.length ; i++) {
-        for(let j = 0 ; i<depar.length ; i++) {
-            if(depar[i].departement==depar[j].departement) {
-                  stockegale=depar[i].name + depar[i].departement
-                console.log(depar[i].nom + stockegale)
-            }
-            else{
-                console.log(depar[i].nom + depar[i].departement)
+
+    let employeedepartment = {};
+
+    for (let i = 0; i < depar.length; i++) {
+
+        let stockegnom = [];
+
+        for (let j = 0; j < depar.length; j++) {
+
+            if (depar[i].departement == depar[j].departement) {
+
+                stockegnom.push(depar[j].nom);
             }
         }
+
+        employeedepartment[depar[i].departement] = stockegnom;
     }
+
+    console.log(employeedepartment);
 }
+
 departement();
+
+//Fusionner les profils : Écrire une fonction qui prend deux objets (par exemple, et ) 
+// et les fusionne en un seul objet.basicInfocontactInfo
+
+let basicInfo = {
+    nom: "Aya",
+    age: 23,
+    ville: "Marrakech"
+};
+let contactInfo = {
+    email: "aya@gmail.com",
+    telephone: "0612345678",
+    adresse: "Guéliz"
+};
+function twooobjet() {
+    let employeedepartment = {};
+     
+    for(let key in basicInfo) {
+    employeedepartment[key] = basicInfo[key];
+    }
+     for(let key in contactInfo) {
+    employeedepartment[key] = contactInfo[key];
+    }
+    console.log(employeedepartment)
+}
+twooobjet()
+
+//Introduction au clone profond : Écrivez une fonction qui crée manuellement une copie
+//  profonde d’un objet simple (sans objets imbriqués) afin que la modification de la 
+// copie n’affecte pas l’original.
