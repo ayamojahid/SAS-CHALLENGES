e40d5b36293fae1fa9d39065a728e89e6a2751a6
Aya <aya.mojahid.2025@gmail.com>
2026-09-08 17:05:20 +0100

    update

---
+++ leschallengequiz.js
@@ -0,0 +1,16 @@
+function inverse() {
+
+let entre="welcome to youcode !"
+
+let inversee=[];
+
+for(let i=entre.length-1 ; i>entre.length ; i-- ) {
+console.log(entre[i])
+inversee.push(entre[i])
+
+}
+
+
+
+}
+inverse();
